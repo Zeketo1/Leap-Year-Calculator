2ccc1aa2db076b35ba8be4b0d0e67b1ba36ba594
Zeketo1 <152497390+Zeketo1@users.noreply.github.com>
2024-02-24 19:43:00 +0100

    First commit

---
+++ Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int choice = Console.Read();
+        int second = choice - 48;
+
+        
+        int currentYear = DateTime.Now.Year;
+
+        int leapYearCount = 0;
+
+        Console.WriteLine("Next 20 Leap Years:");
+
+        while (leapYearCount < second)
+        {
+        
+            if (IsLeapYear(currentYear))
+            {
+                Console.WriteLine(currentYear);
+
+                leapYearCount++;
+            }
+
+            currentYear++;
+        }
+
+        // Console.ReadLine(); 
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+    }
+}
